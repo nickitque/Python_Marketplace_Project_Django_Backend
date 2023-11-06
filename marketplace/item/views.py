431fde1e9d41a6f5a701d8faf8f7fb30b2dfcf17
Nikita Radzisheuski <nikitaradzisheuski@nikitas-macbook-pro.home>
2023-11-06 20:15:12 +0100

    Implemented filter methods and template.

--- marketplace/item/views.py
+++ marketplace/item/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 from .forms import NewItemForm, EditItemForm
-from .models import Item
+from .models import Item, Category
 
-# Create your views here.
+
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
+    })
+
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
@@ -13,6 +33,7 @@ def detail(request, pk):
         'item': item,
         'related_items': related_items,
     })
+
 
 @login_required()
 def new(request):
@@ -33,6 +54,7 @@ def new(request):
         'title': 'New item',
     })
 
+
 @login_required()
 def edit(request, pk):
     item = get_object_or_404(Item, pk=pk, created_by=request.user)
@@ -51,6 +73,7 @@ def edit(request, pk):
         'form': form,
         'title': 'Edit item',
     })
+
 
 @login_required
 def delete(request, pk):
