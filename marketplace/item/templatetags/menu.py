821dbaf85c41361a09102f6d12b6d6c73cfa0425
Nikita <nickitque.39@gmail.com>
2023-12-11 15:19:33 +0300

    Added menu as a templatetags

---
+++ marketplace/item/templatetags/menu.py
@@ -0,0 +1,10 @@
+from django import template
+from item.models import Category
+
+
+register = template.Library()
+
+@register.inclusion_tag('item/menu.html')
+def menu():
+    categories = Category.objects.all()
+    return {'categories': categories}
