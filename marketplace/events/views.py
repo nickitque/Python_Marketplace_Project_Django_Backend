e82598564bed75b2e9627e280d5134043ade02aa
Nikita <nickitque.39@gmail.com>
2023-12-17 21:23:57 +0100

    Added id tags to html, updated events view

--- marketplace/events/views.py
+++ marketplace/events/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Events
 
 
 def index(request):
-    return render(request, 'index.html')
+    all_events = Events.objects.all()
+    context = {
+        "events": all_events,
+    }
+    return render(request, 'index.html', context)
 
+def all_events(request):
+    all_events = Events.objects.all()
+    out = []
+    for event in all_events:
+        out.append({
+            'title': event.name,
+            'id': event.id,
+            'start': event.start.strftime("%m/%d/%Y, %H:%M:%S"),
+            'end': event.start.strftime("%m/%d/%Y, %H:%M:%S"),
+        })
+
+    return JsonResponse(out, safe=False)
