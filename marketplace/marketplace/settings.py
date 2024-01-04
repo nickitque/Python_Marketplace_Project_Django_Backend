d38b09c996e128a4ee549a705a039c0d89252e2f
Nikita <nickitque.39@gmail.com>
2024-01-05 00:43:06 +0100

    Added new blog app. Registered it and added to urls.py

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'dashboard',
     'conversation',
     'events',
+    'blog',
 ]
 
 MIDDLEWARE = [
--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('inbox/', include('conversation.urls')),
     path('', include('events.urls')),
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
