dadf8d2ad1d422beb43891319dbd0e161ea6a81f
Nikita <nickitque.39@gmail.com>
2024-01-05 00:58:20 +0100

    Template edits, added url to header, added app name to urls.py and edited hrefs

--- marketplace/blog/urls.py
+++ marketplace/blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
+
 urlpatterns = [
     path("", views.blog_index, name="blog_index"),
     path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
